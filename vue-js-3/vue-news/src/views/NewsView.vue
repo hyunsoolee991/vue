b6a8dd45ba7ecfc9dc77043024be17ec33231e5e
Hyunsoo Lee <hyunsoolee991@gmail.com>
2023-07-13 19:50:59 +0900

    리팩토링: Mixin으로 페이지 공통화
    하이오더 컴포넌트 (HOC)에 이은 두번째 공통화 방법

---
+++ vue-js-3/vue-news/src/views/NewsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><list-item></list-item></div>
+</template>
+
+<script>
+import ListItem from "../components/ListItem.vue";
+import ListMixIn from "../mixins/ListMixin.js";
+
+export default {
+  components: {
+    ListItem,
+  },
+  mixins: [ListMixIn],
+};
+</script>
+
+<style></style>
