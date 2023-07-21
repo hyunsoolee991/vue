6a648b1b9e7793bd9a39931c63c3a678c4d8bcb9
Hyunsoo Lee <hyunsoolee991@gmail.com>
2023-07-21 22:44:45 +0900

    리팩토링: async, await 로 수정

--- vue-js-3/vue-news/src/store/actions.js
+++ vue-js-3/vue-news/src/store/actions.js
@@ -8,27 +8,34 @@ import {
 } from "../api/index.js";
 
 export default {
-  FETCH_NEWS(context) {
-    return fetchNewsList()
-      .then((response) => {
-        context.commit("SET_NEWS", response.data);
-        return response;
-      })
-      .catch((error) => console.log(error));
+  // promise
+  // FETCH_NEWS(context) {
+  //   return fetchNewsList()
+  //     .then((response) => {
+  //       context.commit("SET_NEWS", response.data);
+  //       return response;
+  //     })
+  //     .catch((error) => console.log(error));
+  // },
+  // async
+  async FETCH_NEWS(context) {
+    const response = await fetchNewsList();
+    context.commit("SET_NEWS", response.data);
+    return response;
   },
-  FETCH_ASK({ commit }) {
-    return fetchAskList()
-      .then(({ data }) => {
-        commit("SET_ASK", data);
-      })
-      .catch((error) => console.log(error));
+  async FETCH_ASK({ commit }) {
+    const response = await fetchAskList();
+    commit("SET_ASK", response.data);
+    return response;
   },
-  FETCH_JOBS({ commit }) {
-    return fetchJobsList()
-      .then(({ data }) => {
-        commit("SET_JOBS", data);
-      })
-      .catch((error) => console.log(error));
+  async FETCH_JOBS({ commit }) {
+    try {
+      const response = await fetchJobsList();
+      commit("SET_JOBS", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
   // #2
   FETCH_LIST({ commit }, pageName) {
